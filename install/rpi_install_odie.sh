6cb2724a16293a4e130b2d6d8b8ed1125b8b200c
Balzano <Andrea.Balzano@au.fujitsu.com>
2017-09-27 12:42:33 +1000

    FIX: debian install script

--- install/rpi_install_odie.sh
+++ install/rpi_install_odie.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-
 # This script will install automatically everything needed for odie
 # usage: ./rpi_install_odie.sh [<branch_name>]
 # E.g: ./rpi_install_odie.sh dev
 # If no branch are set, the master branch will be installed
-
 # name of the branch to install
 branch="master"
-
 # get the branch name to install from passed arguments if exist
 if [ $# -eq 0 ]
   then
@@ -29,7 +26,7 @@ sudo apt-get install -y git python-dev libsmpeg0 libttspico-utils libsmpeg0 \
 flac dialog libffi-dev libffi-dev libssl-dev portaudio19-dev build-essential \
 libssl-dev libffi-dev sox libatlas3-base mplayer libyaml-dev libpython2.7-dev pulseaudio pulseaudio-utils libav-tools \
 postgresql postgresql-contrib cython swig3.0 python-pyaudio python3-pyaudio python3-psycopg2 python3-dialog sox \
-build-essential python-dev git scons
+python-dev git scons
 
 # this is used to help the RPI
 sudo apt-get install -y libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev libatlas-base-dev
@@ -42,6 +39,8 @@ cd rpi_ws281x
 scons
 cd python
 sudo python3 setup.py install
+cd ..
+rm -rf rpi_ws281x
 echo "Cloning the adafruit...[OK]"
 
 echo "Cloning the project"
@@ -58,8 +57,6 @@ echo "Installing Odie...[OK]"
 
 #install postgres dependency
 echo  "configuring postgres dependency..."
-sudo -u postgres psql
-create user admin with superuser password 'secret';
-create database odie owner admin;
-\q
+sudo psql postgres -c "create user admin with superuser password 'secret';"
+sudo psql postgres -c "create database odie owner admin;"
 echo "configuring postgres...[OK]"
